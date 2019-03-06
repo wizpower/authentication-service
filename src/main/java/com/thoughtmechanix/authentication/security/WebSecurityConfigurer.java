f162700228e487df32f2a38569238079146c600c
xj <351756725@qq.com>
2019-03-06 18:54:54 +0800

    Initial commit

---
+++ src/main/java/com/thoughtmechanix/authentication/security/WebSecurityConfigurer.java
@@ -0,0 +1,41 @@
+package com.thoughtmechanix.authentication.security;
+
+import com.thoughtmechanix.authentication.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(new MyPasswordEncoder())
+                .withUser("john.carnell").password("password1").roles("USER")
+                .and()
+                .withUser("william.woodward").password("password2").roles("USER", "ADMIN");
+
+    }
+
+
+}
